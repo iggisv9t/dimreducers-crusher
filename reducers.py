bcbc6d1b28d7e1d288fe09a3212574ecf004039c
Sviatoslav <iggisv9t@gmail.com>
2021-04-02 17:18:50 +0300

    fix trimap transform

--- reducers.py
+++ reducers.py
@@ -8,7 +8,9 @@ class BaseReducer:
     def transform(self, X):
         pass
 
-    def fit_transform(self, X,):
+    def fit_transform(
+        self, X,
+    ):
         self.fit(X)
         return self.transform(X)
 
@@ -16,6 +18,7 @@ class BaseReducer:
 class UmapReducer(BaseReducer):
     def __init__(self, outdim=2, **kwargs):
         import umap
+
         self.reducer = umap.UMAP(n_components=outdim, **kwargs)
 
     def fit(self, X):
@@ -28,10 +31,27 @@ class UmapReducer(BaseReducer):
 class TrimapReducer(BaseReducer):
     def __init__(self, outdim=2, **kwargs):
         import trimap
+
         self.reducer = trimap.TRIMAP(n_dims=outdim)
 
     def fit(self, X):
         self.reducer.fit(X)
 
     def transform(self, X):
-        return self.reducer.transform(X)
+        return self.reducer.embedding_
+
+
+class PyMDENeigh(BaseReducer):
+    def __init__(self, outdim=2, **kwargs):
+        self.outdim = outdim
+        self.kwargs = kwargs
+
+    def fit(self, X):
+        import pymde
+
+        self.reducer = pymde.preserve_neighbors(
+            X, embedding_dim=self.outdim, **self.kwargs
+        )
+
+    def transform(self, X):
+        return self.reducer.embed()
