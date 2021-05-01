b5b96b459506198fbc88322ed16328045591d9b1
Sviatoslav <iggisv9t@gmail.com>
2021-05-01 22:12:35 +0300

    add reports generator

--- run_single.py
+++ run_single.py
@@ -1,6 +1,7 @@
 import os
 import time
 import click
+import json
 from dimreducers_crusher import datasets, metrics, reducers, plotters
 from dimreducers_crusher.utils.py_utils import get_registry
 
@@ -15,6 +16,10 @@ REDUCER_REGISTRY = get_registry(reducers)
 @click.argument("metric_name", type=click.Choice(METRIC_REGISTRY))
 @click.argument("reducer_name", type=click.Choice(REDUCER_REGISTRY))
 def main(dataset_name, metric_name, reducer_name):
+    now = time.strftime("%y%m%d_%H%M%S")
+    report = dict()
+    picsdir = "./pics"
+    repdir = "./reports"
     print("====================")
     print("=====DIMCRUSHER=====")
     print("====================")
@@ -27,19 +32,32 @@ def main(dataset_name, metric_name, reducer_name):
     data_reduced = reducer.fit_transform(data)
     print(data_reduced.shape, data_reduced.min(), data_reduced.max())
 
+    # TODO: Allow multiple metrics
     metric = METRIC_REGISTRY[metric_name]()
     metric_value = metric.score(data, data_reduced)
     print(metric_value)
+    metric_report = {metric_name: metric_value}
 
     # Plotter
+    # TODO: Allow multiple plotters
+    fname = "{}_{}_{}".format(dataset_name, reducer_name, now)
+
     p = plotters.DefaultPlotter()
-    os.makedirs("./pics", exist_ok=True)
-    now = time.strftime("%y%m%d_%H%M%S")
-    savepath = "./pics/{}_{}_{}.png".format(dataset_name, reducer_name, now)
-    p.plot(
-        data_reduced, savepath
-    )
-    # TODO: Add json report
+    os.makedirs(picsdir, exist_ok=True)
+
+    picpath = os.path.join(picsdir, "{}.png".format(fname))
+    p.plot(data_reduced, picpath)
+    
+    # Report
+    os.makedirs(repdir, exist_ok=True)
+    report["dataset"] = dataset_name
+    report["reducer"] = reducer_name
+    report["metrics"] = metric_report
+    report["plots"] = [picpath]
+
+    reppath = os.path.join(repdir, "{}.json".format(fname))
+    with open(reppath, "w") as fp:
+        json.dump(report, fp)
 
 
 if __name__ == "__main__":
